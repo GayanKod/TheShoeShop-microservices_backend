1e5aa93ba4c3ab65451899c9d3a949982388e363
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-11-11 14:34:36 +0530

    OrderService - Create OrderRequest DTO

---
+++ order-service/src/main/java/com/gayankod/theshoeshop/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.gayankod.theshoeshop.orderservice.dto;
+
+import com.gayankod.theshoeshop.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDTO> orderLineItemsDTOList;
+}
