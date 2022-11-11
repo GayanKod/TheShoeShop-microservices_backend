988da65bb75bce7e490d684bf0ec4da0575074ed
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-11-12 01:02:28 +0530

    Modify Inventory Service to Accept collection of request by order service

--- inventory-service/src/main/java/com/gayankod/theshoeshop/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/gayankod/theshoeshop/inventoryservice/service/InventoryService.java
@@ -1,10 +1,13 @@
 package com.gayankod.theshoeshop.inventoryservice.service;
 
+import com.gayankod.theshoeshop.inventoryservice.dto.InventoryResponse;
 import com.gayankod.theshoeshop.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public  boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public  List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder().skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQty() > 0)
+                            .build()
+                ).toList();
     }
 }
