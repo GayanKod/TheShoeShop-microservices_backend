9ae102d6f31c040b3a4c3ebdf66916ee4fd5a3b2
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-11-12 16:13:40 +0530

    Set Circuit Breaker name

--- order-service/src/main/java/com/gayankod/theshoeshop/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/gayankod/theshoeshop/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.gayankod.theshoeshop.orderservice.controller;
 
 import com.gayankod.theshoeshop.orderservice.dto.OrderRequest;
 import com.gayankod.theshoeshop.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="Inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
